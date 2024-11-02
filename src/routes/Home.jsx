bdbc9a6391a1adf7ffb03d812d177a304466cdd7
rafay1234321 <abdulrafaymalik16@gmail.com>
2024-11-02 19:08:18 +0500

    added deleet and edit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -11,9 +11,12 @@ import {
   TextField,
   IconButton,
   Tooltip,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import {
   addDoc,
   collection,
@@ -22,6 +25,9 @@ import {
   orderBy,
   query,
   serverTimestamp,
+  deleteDoc,
+  doc,
+  updateDoc,
 } from "firebase/firestore";
 import { db } from "../pages/confic";
 import { Authcontext } from "../context/Authcontext";
@@ -31,6 +37,9 @@ const Home = () => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const usersRef = useRef([]);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedMessage, setSelectedMessage] = useState(null);
+
   useEffect(() => {
     (async () => {
       const userQuery = query(collection(db, "users"));
@@ -40,40 +49,64 @@ const Home = () => {
           ...usersRef.current,
           [doc.id]: doc.data(),
         };
-        // usersRef.current = [...usersRef.current, { ...doc.data(), id: doc.id }];
       });
       const q = query(collection(db, "messages"), orderBy("sentAt", "asc"));
       const unsubscribe = onSnapshot(q, (snapshot) => {
-        snapshot.docChanges().forEach((change) => {
+        const newMessages = snapshot.docChanges().map((change) => {
           if (change.type === "added") {
-            setMessages((prev) => [
-              ...prev,
-              {
-                ...change.doc.data(),
-                user: usersRef.current[change.doc.data().sentBy],
-              },
-            ]);
+            return {
+              ...change.doc.data(),
+              user: usersRef.current[change.doc.data().sentBy],
+              id: change.doc.id,
+            };
           }
-        });
+          return null;
+        }).filter(Boolean);
+        setMessages((prev) => [...prev, ...newMessages]);
       });
-
-      // const q = query(collection(db, "messages"), orderBy("sentAt", "asc"));
-      // const querySnapshot = await getDocs(q);
-      // querySnapshot.forEach((doc) => {
-      //   const singleMsg = doc.data();
-      //   setMessages((prev) => [...prev, singleMsg]);
-      //   // setMessages(allMessages);
-      // });
     })();
   }, []);
-  console.log(">>>", messages);
+
   const sendMessage = async () => {
     const docRef = await addDoc(collection(db, "messages"), {
       text: message,
       sentBy: loginuser.uid,
       sentAt: serverTimestamp(),
     });
+    setMessage(""); 
   };
+
+  const handleMenuClick = (event, message) => {
+    setAnchorEl(event.currentTarget);
+    setSelectedMessage(message);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+    setSelectedMessage(null);
+  };
+
+  const handleDelete = async () => {
+    if (selectedMessage) {
+      await deleteDoc(doc(db, "messages", selectedMessage.id));
+      handleClose();
+    }
+  };
+
+  const handleUpdate = async (newText) => {
+    if (selectedMessage) {
+      await updateDoc(doc(db, "messages", selectedMessage.id), {
+        text: newText,
+      });
+      setMessages((prev) =>
+        prev.map((msg) =>
+          msg.id === selectedMessage.id ? { ...msg, text: newText } : msg
+        )
+      );
+      handleClose();
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -85,10 +118,7 @@ const Home = () => {
         borderRadius: 2,
       }}
     >
-      <AppBar
-        position="static"
-        sx={{ borderRadius: "8px 8px 0 0", bgcolor: "green" }}
-      >
+      <AppBar position="static" sx={{ borderRadius: "8px 8px 0 0", bgcolor: "green" }}>
         <Toolbar>
           <Avatar sx={{ bgcolor: "white", marginRight: 2 }}>
             <ChatIcon color="primary" />
@@ -110,51 +140,35 @@ const Home = () => {
         }}
       >
         <List sx={{ padding: 0 }}>
-          {messages.map((data, index) => {
-            return (
-              <React.Fragment key={index}>
-                {data.sentBy !== loginuser.uid ? (
-                  <ListItem sx={{ justifyContent: "flex-start" }}>
-                    <Box
-                      sx={{
-                        maxWidth: "75%",
-                        bgcolor: "#e0e0e0",
-                        color: "black",
-                        borderRadius: "16px 16px 16px 0",
-                        padding: "10px 15px",
-                        boxShadow: 1,
-                        wordWrap: "break-word",
-                      }}
-                    >
-                      <Tooltip
-                        title={`${data.user.firstName} ${data.user.lastname}`}
-                      >
-                        <Typography variant="body1">{data.text}</Typography>
-                      </Tooltip>
-                    </Box>
-                  </ListItem>
-                ) : (
-                  <ListItem sx={{ justifyContent: "flex-end" }}>
-                    <Box
-                      sx={{
-                        maxWidth: "75%",
-                        bgcolor: "green",
-                        color: "white",
-                        borderRadius: "16px 16px 0 16px",
-                        padding: "10px 15px",
-                        boxShadow: 1,
-                        wordWrap: "break-word",
-                      }}
-                    >
-                      <Tooltip title="You">
-                        <Typography variant="body1">{data.text}</Typography>
-                      </Tooltip>
-                    </Box>
-                  </ListItem>
+          {messages.map((data) => (
+            <ListItem key={data.id} sx={{ justifyContent: data.sentBy !== loginuser.uid ? "flex-start" : "flex-end" }}>
+              <Box
+                sx={{
+                  maxWidth: "75%",
+                  display:"flex",
+                  bgcolor: data.sentBy !== loginuser.uid ? "#e0e0e0" : "green",
+                  color: data.sentBy !== loginuser.uid ? "black" : "white",
+                  borderRadius: data.sentBy !== loginuser.uid ? "16px 16px 16px 0" : "16px 16px 0 16px",
+                  padding: "10px 15px",
+                  boxShadow: 1,
+                  wordWrap: "break-word",
+                }}
+              >
+                <Tooltip title={`${data.user?.firstName} ${data.user?.lastname}`}>
+                  <Typography variant="body1">{data.text}</Typography>
+                </Tooltip>
+                {data.sentBy === loginuser.uid && ( 
+                  <IconButton
+                    size="small"
+                    onClick={(event) => handleMenuClick(event, data)}
+                    sx={{ marginLeft: 1 }}
+                  >
+                    <MoreVertIcon fontSize="small" />
+                  </IconButton>
                 )}
-              </React.Fragment>
-            );
-          })}
+              </Box>
+            </ListItem>
+          ))}
         </List>
       </Paper>
 
@@ -195,6 +209,24 @@ const Home = () => {
           <SendIcon />
         </IconButton>
       </Box>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={() => {
+            const newText = prompt("Edit your message:", selectedMessage.text);
+            if (newText) {
+              handleUpdate(newText);
+            }
+          }}
+        >
+          Edit
+        </MenuItem>
+        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+      </Menu>
     </Box>
   );
 };
